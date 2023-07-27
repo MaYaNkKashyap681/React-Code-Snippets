ace9fd08f3aaf28d4c9092e0cc7c765ff0e1fd11
Mayank Kashyap <96070627+MaYaNkKashyap681@users.noreply.github.com>
2023-07-27 12:09:09 +0530

    Create GetBoundngClientRect.js

---
+++ GetBoundngClientRect.js
@@ -0,0 +1,29 @@
+  const d = headingRef.current.getBoundingClientRect();
+
+//this will give the position of the element in the page
+
+//Output where x is the length between window left and the element starting same applies to others as per their name
+bottom
+: 
+350.6666717529297
+height
+: 
+100
+left
+: 
+512.1666870117188
+right
+: 
+767.8229370117188
+top
+: 
+250.6666717529297
+width
+: 
+255.65625
+x
+: 
+512.1666870117188
+y
+: 
+250.6666717529297
